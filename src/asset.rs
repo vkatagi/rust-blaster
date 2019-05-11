1675b56e736ad278e647a6ad9b026151bcbc6156
katagis <fox.dealos@gmail.com>
2019-05-11 19:06:22 +0300

    Add asset.rs, small asset library

---
+++ src/asset.rs
@@ -0,0 +1,104 @@
+use std::path;
+use std::collections::HashMap;
+use ggez::Context;
+
+
+use ggez::graphics::Image as ImageAsset;
+use ggez::graphics::Font as FontAsset;
+use ggez::audio::Source as SoundAsset;
+
+
+trait LoadableAsset {
+    fn new(ctx: &mut Context, file: &'static str) -> ggez::GameResult<Self> where Self: Sized;
+}
+
+impl LoadableAsset for ImageAsset {
+    fn new(ctx: &mut Context, file: &'static str) -> ggez::GameResult<ImageAsset> {
+        ImageAsset::new(ctx, file)
+    }
+}
+
+impl LoadableAsset for FontAsset {
+    fn new(ctx: &mut Context, file: &'static str) -> ggez::GameResult<FontAsset> {
+        FontAsset::new(ctx, file, 18)
+    }
+}
+
+impl LoadableAsset for SoundAsset {
+    fn new(ctx: &mut Context, file: &'static str) -> ggez::GameResult<SoundAsset> {
+        SoundAsset::new(ctx, file)
+    }
+}
+
+#[derive(Debug)]
+struct AssetLibrary<T: LoadableAsset> {
+    // TODO: IMPR: This probably should be just copmile-time strings. &'static str (?) as keys
+    assets: HashMap<String, T>,
+}
+
+// Look into why do we have to specify all this info 
+impl<T> AssetLibrary<T> where T: LoadableAsset {
+
+    fn new() -> Self {
+        Self {
+            assets: HashMap::new()
+        }
+    }
+
+    fn load(&mut self, ctx: &mut Context, file: &'static str) -> ggez::GameResult<()> {
+        let asset = LoadableAsset::new(ctx, file)?;
+        self.assets.insert(file.to_owned(), asset);
+        Ok(())
+    }  
+
+    fn get<S>(&self, name: S) -> Option<&T> where S: Into<String> {
+        return self.assets.get(&name.into());
+    }
+}
+
+#[derive(Debug)]
+pub struct Assets {
+    pub images: AssetLibrary<ImageAsset>,
+    pub fonts: AssetLibrary<FontAsset>,
+    pub sounds: AssetLibrary<SoundAsset>,
+}
+
+impl Assets {
+    
+    const fn new(ctx: &mut ggez::Context, files: Vec<&'static str>) -> Self {
+        let images = AssetLibrary::new();
+        let fonts = AssetLibrary::new();
+        let sounds = AssetLibrary::new();
+        
+        for f in files {
+            match f[f.len()-3..] {
+                "png" | "jpg" => images.load(ctx, f).unwrap(),
+                "ogg" => sounds.load(ctx, f).unwrap(),
+                "ttf" => fonts.load(ctx, f).unwrap(),
+            }
+            
+        }
+        
+
+        Self {
+            images: images,
+            fonts: fonts,
+            sounds: sounds,
+        }
+    }
+
+    // I would prefer get<ImageAsset>("myasset.png");
+    // but I could not even find if its possible to do this currently in rust.
+
+    fn get_image<S>(&self, name: S) -> Option<&ImageAsset> where S: Into<String> {
+        self.images.get(name)
+    }
+
+    fn get_font<S>(&self, name: S) -> Option<&FontAsset> where S: Into<String> {
+        self.fonts.get(name)
+    }
+
+    fn get_sound<S>(&self, name: S) -> Option<&SoundAsset> where S: Into<String> {
+        self.sounds.get(name)
+    }
+}
